46a36282d8be2c661a7ad6df165495cf9e48f6c3
Prahit Yaugand <52546862+prahity@users.noreply.github.com>
2024-03-20 08:23:55 -0700

    Update TreeFractal.java

--- fractals/TreeFractal/TreeFractal.java
+++ fractals/TreeFractal/TreeFractal.java
@@ -22,23 +22,25 @@ class Tree extends JPanel {
     public void paintComponent(Graphics g) {
 
         //Initializing variables
-        int length = 225;
+        int length = 200;
         int midX = 450;
         int bottomY = 750;
-        double angle = -Math.PI / 2;
+        double angle = -Math.PI/2;
+        double angleChange = Math.PI/6;
 
         //Drawing base of tree
         g.setColor(Color.white);
         g.drawLine(midX,bottomY,midX,bottomY - length);
 
         //Calling recursive function for rest of tree
-        paintLinesRecurse(g,midX,bottomY - length,length / 1.5,angle);
+
+        paintLinesRecurse(g,midX,bottomY - length,length / 1.5,angle,angleChange);
     }
 
-    public void paintLinesRecurse(Graphics g,int startX,int startY,double length,double angle) {
+    public void paintLinesRecurse(Graphics g,int startX,int startY,double length,double angle,double angleChange) {
 
         //Initializing variables
-        double division = 1.4;
+        double division = 1.4;//Math.random() / 10 + 1.3;
 
         // Base case (stops when length is too small)
         if(length < 0.5) {
@@ -46,17 +48,16 @@ class Tree extends JPanel {
         }
 
         //Recursive case for left tree side
-        int x =(int)( Math.cos(angle - Math.PI / 6) * length);
-        int y =(int)( Math.sin(angle - Math.PI / 6) * length);
+        int x =(int)( Math.cos(angle - angleChange) * length);
+        int y =(int)( Math.sin(angle - angleChange) * length);
         g.drawLine(startX,startY,startX + x,startY + y);
-        paintLinesRecurse(g,startX + x,startY + y,(length / division),angle - Math.PI/6);
+        paintLinesRecurse(g,startX + x,startY + y,(length / division),angle - angleChange,angleChange);
 
         //Recursive case for right tree side
-        x =(int)( Math.cos(angle + Math.PI / 6) * length);
-        y =(int)( Math.sin(angle + Math.PI / 6) * length);
+        x =(int)( Math.cos(angle + angleChange) * length);
+        y =(int)( Math.sin(angle + angleChange) * length);
         g.drawLine(startX,startY,startX + x,startY + y);
-        paintLinesRecurse(g,startX + x,startY + y,(length / division),angle + Math.PI/6);
+        paintLinesRecurse(g,startX + x,startY + y,(length / division),angle + angleChange,angleChange);
     }
 }
-
 
